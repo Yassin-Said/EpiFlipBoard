318dde08439065ab1f6c0985cd0ba526dd5f7b67
Yassin-Said <said.yassin@epitech.eu>
2025-12-02 10:32:07 +0100

    [ADD] - strating fastAPI googlefont dependency

--- back-end/main.py
+++ back-end/main.py
@@ -1,13 +1,24 @@
 from typing import Union
-
+import json
+import psycopg2
 from fastapi import FastAPI
+from supabase import create_client, Client
+
+url = "https://cnloedhcnffbfucxoiqb.supabase.co/"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNubG9lZGhjbmZmYmZ1Y3hvaXFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4NTA5OTEsImV4cCI6MjA3ODQyNjk5MX0.RCPogOS3JztbWnErbqKP3SUorR2jwUnOjME3VExc19Q"
+
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    try:
+        supabase: Client = create_client(url, key)
+        data = supabase.table("profiles").select("*").execute()
+        return data.data
+
+    except Exception as e:
+        print(f"Failed to connect: {e}")
 
 
 @app.get("/items/{item_id}")
