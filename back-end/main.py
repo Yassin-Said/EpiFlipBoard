48d4efbfd90959bd6eec386ab8ff7e1a3347a3af
Yassin-Said <said.yassin@epitech.eu>
2025-12-09 16:02:06 +0100

    [UPD] - Make it work for render

--- back-end/main.py
+++ back-end/main.py
@@ -1,25 +1,21 @@
 from typing import Union
-import json
-import psycopg2
 from fastapi import FastAPI
-from supabase import create_client, Client
-
-url = "https://cnloedhcnffbfucxoiqb.supabase.co/"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNubG9lZGhjbmZmYmZ1Y3hvaXFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4NTA5OTEsImV4cCI6MjA3ODQyNjk5MX0.RCPogOS3JztbWnErbqKP3SUorR2jwUnOjME3VExc19Q"
-
+from supabase import create_client
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     try:
-        supabase: Client = create_client(url, key)
+        url = os.environ["SUPABASE_URL"]
+        key = os.environ["SUPABASE_KEY"]
+
+        supabase = create_client(url, key)
         data = supabase.table("profiles").select("*").execute()
         return data.data
-
     except Exception as e:
-        print(f"Failed to connect: {e}")
-
+        return {"error": str(e)}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
